34f7e56b584f640fbe82cae7b5ff5a533b110320
AKILAN SP <akilanpugal007@gmail.com>
2026-06-14 17:22:48 +0530

    part 2

--- ex1/IMDB.py
+++ ex1/IMDB.py
@@ -1,5 +1,7 @@
 import pandas as p
 from collections import Counter
+from wordcloud import WordCloud
+import matplotlib.pyplot as pl
 data=p.read_csv('IMDB Dataset.csv')
 print(data.head(10))
 #number of sentences
@@ -17,3 +19,32 @@ top=wc.most_common(10)
 print("top 10 words : ")
 for w,count in top:
     print(f" '{w}'  with frequency {count}")
+#vocabulary size
+s=set(words)
+vsize = len(s)
+print("Vocabulary size :", vsize)
+#indentifying the stop words
+stop_words = {
+    'the','is','in','and','to','of','a','for','on','with',
+    'this','that','it','as','an','are','was','were','be'
+}
+fwords = [w for w in words if w not in stop_words]
+fseries = p.Series(fwords)
+t = fseries.value_counts().head(10)
+print("\nTop 10 AFTER removing stop words:\n", t)
+#average line length
+alen=len(words)/len(sent2)
+print("\nAverage line length :", alen)
+#wordcloud
+clean_text = " ".join(fwords)
+
+wordcloud = WordCloud(
+    width=800,
+    height=400,
+    background_color='white'
+).generate(clean_text)
+
+pl.figure(figsize=(10,5))
+pl.imshow(wordcloud)
+pl.axis("off")
+pl.show()
