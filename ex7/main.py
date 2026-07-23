3ba5f0530b6d0b8843da315adf9e414f50c6267e
AKILAN SP <akilanpugal007@gmail.com>
2026-07-23 21:00:14 +0530

    ex7

--- ex7/main.py
+++ ex7/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import (
@@ -31,38 +32,64 @@ df = pd.DataFrame({
 df["result"] = df["result"].map({"Fail": 0, "Pass": 1})
 print("Sample Data:\n", df.head())
 
+
 X = df.drop(["result", "internal_marks"], axis=1)  # removed leakage feature
 y = df["result"]
+
 
 # Train-Test Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-#decision tree
-dt_full = DecisionTreeClassifier(random_state=42)
-dt_full.fit(X_train, y_train)
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train, y_train)
 
-y_pred_full = dt_full.predict(X_test)
+y_train_pred = model.predict(X_train)
+y_test_pred = model.predict(X_test)
+
+train_accuracy = accuracy_score(y_train, y_train_pred)
+test_accuracy = accuracy_score(y_test, y_test_pred)
+
 print("\n--- Decision Tree (No Restriction) ---")
-print("Accuracy :", accuracy_score(y_test, y_pred_full))
-print("Precision:", precision_score(y_test, y_pred_full))
-print("Recall   :", recall_score(y_test, y_pred_full))
-print("F1 Score :", f1_score(y_test, y_pred_full))
-cm_full = confusion_matrix(y_test, y_pred_full)
+print("Training Accuracy :", train_accuracy)
+print("Testing Accuracy  :", test_accuracy)
+
+if train_accuracy > test_accuracy:
+    print("Model is Overfitting")
+else:
+    print("Model is Not Overfitting")
+print("Accuracy :", accuracy_score(y_test, y_test_pred))
+print("Precision:", precision_score(y_test, y_test_pred))
+print("Recall   :", recall_score(y_test, y_test_pred))
+print("F1 Score :", f1_score(y_test, y_test_pred))
+
+cm_full = confusion_matrix(y_test, y_test_pred)
 print("Confusion Matrix:\n", cm_full)
-print("Classification Report:\n",
-      classification_report(y_test, y_pred_full))
-#pre prunned
-dt_pruned = DecisionTreeClassifier(max_depth=3, random_state=42)
+
+print("Classification Report:\n")
+print(classification_report(y_test, y_test_pred))
+
+dt_pruned = DecisionTreeClassifier(max_leaf_nodes=3,criterion="entropy", random_state=42)
 dt_pruned.fit(X_train, y_train)
-y_pred_pruned = dt_pruned.predict(X_test)
-print("\n--- Pre-Pruned Decision Tree (max_depth=3) ---")
-print("Accuracy :", accuracy_score(y_test, y_pred_pruned))
-print("Precision:", precision_score(y_test, y_pred_pruned))
-print("Recall   :", recall_score(y_test, y_pred_pruned))
-print("F1 Score :", f1_score(y_test, y_pred_pruned))
-cm_pruned = confusion_matrix(y_test, y_pred_pruned)
+
+y_train_pruned = dt_pruned.predict(X_train)
+y_test_pruned = dt_pruned.predict(X_test)
+
+train_acc_pruned = accuracy_score(y_train, y_train_pruned)
+test_acc_pruned = accuracy_score(y_test, y_test_pruned)
+
+print("\n--- Pre-Pruned Decision Tree  ---")
+print("Training Accuracy :", train_acc_pruned)
+print("Testing Accuracy  :", test_acc_pruned)
+
+print("Accuracy :", accuracy_score(y_test, y_test_pruned))
+print("Precision:", precision_score(y_test, y_test_pruned))
+print("Recall   :", recall_score(y_test, y_test_pruned))
+print("F1 Score :", f1_score(y_test, y_test_pruned))
+
+cm_pruned = confusion_matrix(y_test, y_test_pruned)
 print("Confusion Matrix:\n", cm_pruned)
-print("Classification Report:\n",
-      classification_report(y_test, y_pred_pruned))
+
+print("Classification Report:\n")
+print(classification_report(y_test, y_test_pruned))
