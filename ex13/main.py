f892b85b771e7c9bdedd9969ab69c1ff5ddfed17
AKILAN SP <akilanpugal007@gmail.com>
2026-09-15 20:04:24 +0530

    updated keras file

--- ex13/main.py
+++ ex13/main.py
@@ -3,14 +3,14 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 model = tf.keras.models.load_model("mnist_model.keras")
-img = Image.open("4.png").convert("L")
+img = Image.open("5.png").convert("L")
 img = img.resize((28, 28))
 img_array = np.array(img)
 img_array = 255 - img_array
 img_array = img_array / 255.0
-plt.imshow(img_array, cmap="gray")
-plt.title("Input Digit")
-plt.show()
+#plt.imshow(img_array, cmap="gray")
+#plt.title("Input Digit")
+#plt.show()
 img_array = img_array.reshape(1, 28, 28)
 prediction = model.predict(img_array)
 predicted_digit = np.argmax(prediction)
